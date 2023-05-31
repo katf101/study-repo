6d2bf82f570832cd3a41dbd02df7105c0e3d6984
Hansol <102638663+katf101@users.noreply.github.com>
2023-06-01 00:11:35 +0900

    Image 컴포넌트 기능 확인

--- NextJS_확장성_높은_커머스_구축/blog_project/pages/posts/first-post.js
+++ NextJS_확장성_높은_커머스_구축/blog_project/pages/posts/first-post.js
@@ -1,5 +1,14 @@
 import React from "react";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function Firstpost() {
-  return <div>첫번째 글</div>;
+  return (
+    <Layout>
+      <Head>
+        <title>첫번째 글</title>
+      </Head>
+      <h1>첫번째 글</h1>
+    </Layout>
+  );
 }
